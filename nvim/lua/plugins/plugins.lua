d5009748004e669a4b2ec42aaf5f47982c1e38f2
Rak Laptudirm <rak@laptudirm.com>
2025-01-02 18:12:50 +0530

    chore: simpler neovim configuration

---
+++ nvim/lua/plugins/plugins.lua
@@ -0,0 +1,182 @@
+return {
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts_extend = { "spec" },
+		opts = {
+			spec = {
+				{
+					mode = { "n", "v" },
+					{ "<leader>f", group = "file/find" },
+					{ "<leader>s", group = "search" },
+					{
+						"<leader>b",
+						group = "buffer",
+						proxy = "<c-b>",
+						expand = function()
+							return require("which-key.extras").expand.buf()
+						end,
+					},
+					{
+						"<leader>w",
+						group = "windows",
+						proxy = "<c-w>",
+						expand = function()
+							return require("which-key.extras").expand.win()
+						end,
+					},
+				},
+				{
+					mode = { "n" },
+					{ "<leader>bp", "<cmd>bprevvious<cr>", desc = "Prev Buffer" },
+					{ "<leader>bn", "<cmd>bnext<cr>", desc = "Next Buffer" },
+					{ "<leader>bb", "<cmd>e #<cr>", desc = "Switch to Other Buffer" },
+					{ "<leader>bd", "<cmd>bd<cr>", desc = "Delete Buffer" },
+				},
+			},
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		cmd = "Neotree",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>fe",
+				function()
+					require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+				end,
+				desc = "NeoTree",
+			},
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false, -- last release is way too old and doesn't work on Windows
+		build = ":TSUpdate",
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			local mason = require("mason")
+
+			local servers = {
+				yamlls = {},
+				gopls = {},
+			}
+			mason.setup()
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>fr",
+				function()
+					require("conform").format({ async = true, lsp_format = "fallback" })
+				end,
+				mode = "",
+				desc = "Format File",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			format_on_save = function(bufnr)
+				return {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				}
+			end,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				json = { "prettier" },
+				go = { "goimports", "gofumpt" },
+			},
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+				},
+			})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("telescope").setup({})
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search Grep" })
+			vim.keymap.set("n", "<leader>bf", builtin.buffers, { desc = "Open Buffers" })
+			local fk_opts = {
+				cwd = "~/.config/nvim",
+				results_title = "Config",
+			}
+			vim.keymap.set("n", "<leader>fc", function()
+				builtin.find_files(fk_opts)
+			end, { desc = "Search Config Files" })
+		end,
+	},
+}
